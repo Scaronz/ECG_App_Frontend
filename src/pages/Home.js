462bf9ad2a71084b0a903257e606570c75c07735
Scaronz <yahiayakh@gmail.com>
2024-05-27 10:20:51 +0100

    Added select model button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,34 @@
 import React, { useState } from 'react';
-import '../Home.css'; // Import your CSS file
-import Button from '@mui/material/Button'; // Import Material-UI Button
-import InputFileUpload from '../Components/InputFileUpload'; // Import the new file upload component
+import '../Home.css';
+import Button from '@mui/material/Button';
+import InputFileUpload from '../Components/InputFileUpload';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 
 const Home = () => {
   const [uploadedFile, setUploadedFile] = useState(null);
   const [predictionLabel, setPredictionLabel] = useState('');
+  const [selectedModel, setSelectedModel] = useState('');
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     setUploadedFile(file);
-    // Additional logic to handle file upload
   };
 
   const handlePredict = () => {
-    // Additional logic for prediction
+    // Here you would add the prediction logic
     setPredictionLabel('Prediction Result');
+  };
+
+  const handleModelChange = (event) => {
+    setSelectedModel(event.target.value);
   };
 
   return (
     <div className="container">
-      <h1 className="header">ECG Classifier</h1> {/* Header added here */}
+      <h1 className="header">ECG Classifier</h1>
       <div className="upload-section">
         <InputFileUpload onFileChange={handleFileUpload} />
         {uploadedFile && (
@@ -29,9 +37,24 @@ const Home = () => {
           </div>
         )}
       </div>
+      <div className="model-selection-section">
+        <FormControl variant="outlined" className="form-control">
+          <InputLabel id="model-select-label">Select Model</InputLabel>
+          <Select
+            labelId="model-select-label"
+            value={selectedModel}
+            onChange={handleModelChange}
+            label="Select Model"
+          >
+            <MenuItem value={"model1"}>Model 1</MenuItem>
+            <MenuItem value={"model2"}>Model 2</MenuItem>
+            <MenuItem value={"model3"}>Model 3</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
       <div className="predict-section">
         <div className="button-container">
-          <Button variant="contained" onClick={handlePredict}>Predict</Button>
+          <Button variant="contained" onClick={handlePredict} disabled={!selectedModel}>Predict</Button>
           {predictionLabel && (
             <div className="prediction-label">
               Prediction: {predictionLabel}
